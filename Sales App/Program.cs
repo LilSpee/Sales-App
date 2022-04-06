b92b0315c2cf0ad71051d3cb36cd6c866e2b9a8f
4400112554 <4400112554@BT144-PROG23>
2022-04-06 17:58:22 -0600

    Add project files.

---
+++ Sales App/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Sales_App
+{
+    class Program
+    {
+
+        public Program()
+        {
+        
+        }
+        
+        static void Main(string[] args)
+        {
+            double Item1price = 239.99;
+            double Item2price = 129.75;
+            double Item3price = 99.95;
+            double Item4price = 350.89;
+
+            Console.WriteLine("How many of item 1 did you sell?");
+            double item1 = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine("How many of item 2 did you sell?");
+            double item2 = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine("How many of item 3 did you sell?");
+            double item3 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("How many of item 4 did you sell?");
+            double item4 = Convert.ToInt32(Console.ReadLine());
+
+            double Total1 = Item1price * item1;
+            double Total2 = Item2price * item2;
+            double Total3 = Item3price * item3;
+            double Total4 = Item4price * item4;
+            double Totalall = Total1 + Total2 + Total3 + Total4 * .09 + 200;
+
+            Console.WriteLine(Totalall);
+            
+        }
+    }
+}
